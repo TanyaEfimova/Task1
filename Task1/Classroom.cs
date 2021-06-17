597f89709ebee098636181e4de21cbd7bc1f0ead
TanyaEfimova <mail5x5@mail.ru>
2021-06-17 15:21:57 +0300

    Добавьте файлы проекта.

---
+++ Task1/Classroom.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Task1
+{
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            string[] result = (from classroom in classes
+                               from student in classroom.Students
+                               select student).ToArray();
+
+            return result;
+        }
+    }
+}
